44ec83f495fd44da4d5400125570d8b3a8fcb83f
jonasnunn <jonasnunn@gmail.com>
2023-10-14 17:03:22 -0600

    its working

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -2,27 +2,94 @@ const mongodb = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection("favorites").find();
+  const result = await mongodb
+    .getDb()
+    .db("favorites")
+    .collection("favorites")
+    .find();
   result.toArray().then((lists) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
+  // console.log(result);
 };
 
 const getSingle = async (req, res, next) => {
-  const userId = new ObjectId(req.params.id);
+  const bookId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db()
+    .db("favorites")
     .collection("favorites")
-    .find({ _id: userId });
+    .find({ _id: bookId });
   result.toArray().then((lists) => {
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists[0]);
   });
 };
 
+const postBook = async (req, res, next) => {
+  const newContact = {
+    title: req.body.title,
+    author: req.body.author,
+    ranking: req.body.ranking,
+  };
+  const result = await mongodb
+    .getDb()
+    .db("favorites")
+    .collection("favorites")
+    .insertOne(newContact);
+  if (result.acknowledged) {
+    res.status(201).json(result);
+  } else {
+    res
+      .status(500)
+      .json(
+        result.error || "ERROR: Something went wrong, this book was not added."
+      );
+  }
+};
+
+const editBook = async (req, res, next) => {
+  const bookId = new ObjectId(req.params.id);
+  const book = {
+    title: req.body.title,
+    author: req.body.author,
+    ranking: req.body.ranking,
+  };
+  const result = await mongodb
+    .getDb()
+    .db("favorites")
+    .collection("favorites")
+    .replaceOne({ _id: bookId }, book);
+  if (result.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || "ERROR: The book could not be updated.");
+  }
+};
+
+const deleteBook = async (req, res, next) => {
+  const bookId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDb()
+    .db("favorites")
+    .collection("favorites")
+    .deleteOne({ _id: bookId }, true);
+  if (result.deletedCount > 0) {
+    res.status(200).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || "ERROR: The contact was not able to be deleted");
+  }
+};
+
 module.exports = {
-    getAll,
-    getSingle
+  getAll,
+  getSingle,
+  postBook,
+  editBook,
+  deleteBook
 };
--- routes/favorites.js
+++ routes/favorites.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const favController = require("../controllers/favorites.js")
+const favController = require("../controllers/favorites")
 
 router.get('/', favController.getAll);
 router.get('/:id', favController.getSingle);
+router.post('/', favController.postBook);
+router.put('/:id', favController.editBook);
+router.delete('/:id', favController.deleteBook);
 
 module.exports = router;
