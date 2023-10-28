4b66091b1f55f12a8bf04cabcd7e9051f203d237
jonasnunn <jonasnunn@gmail.com>
2023-10-28 17:05:50 -0600

    added some oauth code

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const mongodb = require("./db/connect");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
 const cors = require("cors");
+const session = require("express-session");
+const passport = require("passport");
 const {} = './validation.js';
 
 
@@ -29,3 +31,22 @@ mongodb.initDb((err, mongodb) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
+
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets"
+},
+function(accessToken, refreshToken, profile, cb) {
+  User.findOrCreate(
+      { 
+          googleId: profile.id
+      }, 
+      function (err, user) {
+          return cb(err, user);
+      }
+  );
+}
+));
