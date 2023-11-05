525d6b59ee27bf21b00ba6f9574ed807397ce0a2
jonasnunn <jonasnunn@gmail.com>
2023-11-04 22:41:12 -0600

    fixes

--- routes/books.js
+++ routes/books.js
@@ -4,10 +4,10 @@ const bookController = require("../controllers/books")
 const validation = require("../middleware/validate")
 
 // Book routes
-router.get('/', bookController.getAll);
-router.get('/:id', validation.validateId, bookController.getSingle);
-router.post('/', validation.validateBook, bookController.postBook);
-router.put('/:id', validation.validateBook, bookController.editBook);
-router.delete('/:id', validation.validateId ,bookController.deleteBook);
+router.get('/', validation.requiresAuth(), bookController.getAll);
+router.get('/:id', validation.requiresAuth(), validation.validateId, bookController.getSingle);
+router.post('/', validation.requiresAuth(), validation.validateBook, bookController.postBook);
+router.put('/:id', validation.requiresAuth(), validation.validateBook, bookController.editBook);
+router.delete('/:id', validation.requiresAuth(), validation.validateId ,bookController.deleteBook);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,16 +5,7 @@ const mongodb = require("./db/connect");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
 const cors = require("cors");
-// const session = require("express-session");
-// const MongoDBStore = require('connect-mongodb-session')(session);
 const {} = './validation.js';
-
-
-// const store = new MongoDBStore({
-//   uri: process.env.MONGODB_URI,
-//   collection: 'sessions', // Change this to your desired collection name
-// });
-
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -28,14 +19,6 @@ app
   .use("/", require("./routes"))
   .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
   .use(cors())
-  // .use(
-  //   session({
-  //     secret: '123456789',
-  //     resave: false,
-  //     saveUninitialized: false,
-  //     store: store,
-  //   })
-  // );
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
@@ -45,7 +28,4 @@ mongodb.initDb((err, mongodb) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
-
-
-
 
